0647702730018d1872bdc5eaa693e8380d984f95
FaresAbdelrazek <fares.abdelrazekk@gmail.com>
2025-03-05 13:55:21 +0200

    user modified

--- models/User.js
+++ models/User.js
@@ -1,1 +1,28 @@
-//klam
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profilePicture: {
+    type: String, // URL of the profile picture
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["Standard User", "Organizer", "Admin"],
+    default: "Standard User",
+  },
+}, { timestamps: true }); // Automatically adds createdAt and updatedAt
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
